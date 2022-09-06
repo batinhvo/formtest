57fe3ded0ee072209d60f5250481934579cb14fb
unknown <kd03102014@gmail.com>
2022-09-07 02:06:08 +0700

    update file html 7/9/2022

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ $(document).ready(function() {
     $('.multiselect').multiselect();
     $('.datepicker').datepicker();  
   });
+
+//  function validate(){
+  
+//     if(document.myForm.first_name.value == ""){
+//       alert("Please provide your name! ");
+//       document.myForm.first_name.forcus();
+//       return false;
+//     }
+//     return true;
+//  }
+
+ 
